f2c6a61acd610c6f92e2d8a2176f9642e85d3907
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2025-06-06 23:33:31 -0300

    fiz a busca

--- teste.py
+++ teste.py
@@ -8,13 +8,19 @@ def constroi_indice(arq: io.BufferedReader) -> list[tuple[int, int]]:
     '''
     forma uma tupla com os ids e os offsets
     '''
-    num_registros = int.from_bytes(arq.read(4))
+    led = int.from_bytes(arq.read(4), signed=True)
+    offset = arq.tell()
+    arq.seek(0, os.SEEK_END)
+    offset_final = arq.tell()
+    arq.seek(offset)
     chaves: list[tuple[int, int]] = []
-    for _ in range(num_registros):
+    while offset < offset_final:
         offset = arq.tell()
         dados, tamanho = leia_reg(arq)
-        id = (dados.split('|'))[0]
-        chaves.append((id, offset))
+        id = dados.split('|')[0]
+        if id.isdigit():
+            id_int = int(id)
+            chaves.append((id_int, offset))
     chaves.sort()
     return chaves
 
@@ -28,9 +34,50 @@ def leia_reg(arq: io.BufferedReader) -> tuple[str, int]:
 def busca():
     pass
 
+def busca_binaria(chave: int, indice: list[tuple[int, int]]) -> int:
+    i = 0
+    f = len(indice) - 1
+    while i <= f:
+        m = (i + f)//2
+        if indice[m][0] == chave:
+            return indice[m][1]
+        if indice[m][0] < chave:
+            i = m + 1
+        else: 
+            f = m - 1
+    return -1
+
+def le_e_imprime(arq: io.BufferedReader, offset: int) -> None:
+    arq.seek(offset)
+    dados, tamanho = leia_reg(arq)
+    registros = dados.split('|')
+    for registro in registros:
+        print(registro)
+
+
+
+
+
+
+def le_e_imprime_led(arq: io.BufferedReader) -> None:
+    # talvez separar a função em duas 
+    arq.seek(0)
+    cabeca = int.from_bytes(arq.read(4), signed=True)
+    led: list[int] = []
+    led.append(cabeca)
+    while cabeca != -1:
+        arq.seek(cabeca)
+        arq.read(3)
+        cabeca = int.from_bytes(arq.read(4), signed=True)
+        led.append(cabeca)
+    
+
+
 if __name__ == '__main__':
     filmes = open('filmes.dat', 'rb')
-    print(constroi_indice(filmes))
-    print("oi")
+    indices = constroi_indice(filmes)
+    le_e_imprime(filmes, busca_binaria(29, indices))
+
+    #print(int.from_bytes(filmes.read(4), signed=True))
 
 
